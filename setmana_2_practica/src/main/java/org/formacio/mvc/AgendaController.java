9bdf05b1b12ec35e5f1aa0990f2e45ba54d83d68
marc monjon <sysmarc13@hotmail.com>
2019-04-28 14:06:20 +0200

    Segundo y tercer caso test pasado

--- setmana_2_practica/src/main/java/org/formacio/mvc/AgendaController.java
+++ setmana_2_practica/src/main/java/org/formacio/mvc/AgendaController.java
@@ -1,25 +1,46 @@
 package org.formacio.mvc;
 
 import org.formacio.repositori.AgendaService;
+import org.formacio.repositori.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(path = "/")
 public class AgendaController {
-	
+
 	@Autowired
 	AgendaService agenda;
 
-	
 	@RequestMapping(path = "/nombre")
 	@ResponseBody
-	
 	public int cantidadContactos() {
 
 		return agenda.nombreContactes();
 	}
 
+	@RequestMapping(path = "/telefon")
+	@ResponseBody
+	public String getTelefono(@RequestParam String id) {
+
+		return agenda.recupera(id).getTelefon();
+	}
+
+	@RequestMapping(path = "/contacte/{id}")
+	@ResponseBody
+	public Persona getPersona(@PathVariable String id) {
+		
+		if(agenda.recupera(id)!= null) {
+			
+			return agenda.recupera(id);
+		}
+		else {
+			return null;
+		}
+
+	}
 }
